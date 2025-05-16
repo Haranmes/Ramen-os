abdac5f58c3f4a2c7ce4657c3cf40ab7f97eecd9
Znmin <soliman.ramez@proton.me>
2025-05-16 22:01:00 +0200

    first commit

--- kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b.d
+++ kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b.d
@@ -1,5 +1,5 @@
-/home/rsoliman/Work/limine-rust-template/kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b.d: build.rs
+/home/rsoliman/Work/limine-ramen-os/kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b.d: build.rs
 
-/home/rsoliman/Work/limine-rust-template/kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b: build.rs
+/home/rsoliman/Work/limine-ramen-os/kernel/target/debug/build/limine-rust-template-cc94a14e054a328b/build_script_build-cc94a14e054a328b: build.rs
 
 build.rs:
--- kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426.d
+++ kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426.d
@@ -1,5 +1,5 @@
-/home/rsoliman/Work/limine-rust-template/kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426.d: src/main.rs
+/home/rsoliman/Work/limine-ramen-os/kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426.d: src/main.rs
 
-/home/rsoliman/Work/limine-rust-template/kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426: src/main.rs
+/home/rsoliman/Work/limine-ramen-os/kernel/target/x86_64-unknown-none/debug/deps/limine_rust_template-2363a8fa48f8a426: src/main.rs
 
 src/main.rs:
